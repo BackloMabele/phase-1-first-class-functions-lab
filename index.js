0f6d975a6f62a7b306e43c3b7f53150c23a73dde
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-20 14:50:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  const fareDoubler = function(fare) {
+      return fare * 2;
+  };
+  const fareTripler = function(fares) {
+    return fares * 3;
+};
+function selectDifferentDrivers(drivers, numberOfDrivers) {
+    if (numberOfDrivers === returnLastTwoDrivers) {
+        return drivers.slice(-2)
+    }
+    else if (numberOfDrivers === returnFirstTwoDrivers) {
+        return drivers.slice(0,2)
+    }
+    else {
+        return "sorry"
+    }
+}
